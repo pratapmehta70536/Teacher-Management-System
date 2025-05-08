876ab99261548f08a508c306cdf11a66c18f452f
Pratap Mehta <148173200+kri70536@users.noreply.github.com>
2025-05-08 07:49:21 +0545

    Update DatabaseConnection.java

--- src/main/java/com/teachermanagement/util/DatabaseConnection.java
+++ src/main/java/com/teachermanagement/util/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/teacher_management";
     private static final String USER = "root";
-    private static final String PASSWORD = ""; // Replace with your MySQL password
+    private static final String PASSWORD = ""; 
 
     public static Connection getConnection() throws SQLException {
         try {
